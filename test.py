463bde8e669619cf6fb3f433ccac04c4891e2ede
Susmit Neogi <susmitneogi1@gmail.com>
2024-05-21 22:22:48 +0530

    update

--- test.py
+++ test.py
@@ -9,4 +9,10 @@ tokeniser=SimpleTokeniser(path_to_text=path_to_txt)
 
 ## What is the encoded ID for 'is'?
 
-print(tokeniser.encode('is'))
+print(tokeniser.encode('is'), '\n')
+
+
+## What is the word corresponding to the token 111
+
+
+print(tokeniser.decode(111), '\n')
