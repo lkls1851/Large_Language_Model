72ff16f140f96ad637a40d1b4ef833b8ac0b9333
Susmit Neogi <susmitneogi1@gmail.com>
2024-05-21 21:51:44 +0530

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -6,12 +6,23 @@ import re
 f=open('verdict.txt', 'r')
 text=f.read()
 num_char=len(text)
-print(num_char,'\n')
-print(text[:99], '\n')
+# print(num_char,'\n')
+# print(text[:99], '\n')
 
 ## Splitting by punctuations
 result=re.split(r'([:,;.!?"()\']|--|\s)', text)
-print(result[:10])
+# print(result[:10])
 
 ## Removing whitespaces
 result=[el.strip() for el in result if el.strip()]
+
+# print(result[:99])
+
+## Converting tokens to ID
+
+unique_tokens=sorted(list(set(result)))
+token_id={}
+for i in range(len(unique_tokens)):
+    token_id[unique_tokens[i]]=i
+
+print(token_id)
