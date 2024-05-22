94ea471d6a7fae0bd71f162af3dad206df90905d
Susmit Neogi <susmitneogi1@gmail.com>
2024-05-22 22:11:02 +0530

    updated tokeniser with unknown and eot tokens

---
+++ tokeniserv2.py
@@ -0,0 +1,46 @@
+import numpy as np
+import re
+import os
+
+class SimpleTokeniserv2():
+    def __init__(self, path_to_text):
+        self.path=path_to_text
+        with open(self.path, 'r') as f:
+            self.vocab=f.read()
+        self.red_tokens=re.split(r'([:,;.!?"()\']|--|\s)', self.vocab)
+        self.tokens=sorted(list(set([el.strip() for el in self.red_tokens if el.strip()])))
+        self.tokens.extend(['<unk>', '<endoftext>'])
+        self.hashmap={token:index for index,token in enumerate(self.tokens)}
+        self.inverse_hash={index:token for index, token in enumerate(self.tokens)}
+
+    def encode(self, x):
+
+        tokens=re.split(r'([:,;.!?"()\']|--|\s)', x)
+        tokens=[el.strip() for el in tokens if el.strip()]
+        ids=[]
+        for el in tokens:
+        
+            if el not in self.hashmap.keys():
+                ids.append(self.hashmap['<unk>'])
+        
+            else:
+                ids.append(self.hashmap[el])
+        
+        return ids
+    
+    def decode(self, id):
+
+        string=''
+        if type(id)==int:
+            meta_list=[]
+            meta_list.append(id)
+            id=meta_list
+
+
+        for el in id:
+
+
+            string+=(self.inverse_hash[el]+' ')
+                
+
+        return string
