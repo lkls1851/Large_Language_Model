cce4058bfbe9f19fb37cf45c3db758483bab6b54
Susmit Neogi <susmitneogi1@gmail.com>
2024-06-17 12:18:42 +0530

    added feature for calculating attn scores

--- attentionv1.py
+++ attentionv1.py
@@ -1,10 +1,14 @@
 import numpy as np
+import torch
 class Attentionv1():
     def __init__(self, inputs):
         self.input=inputs
     def attn_scores(self, query):
         res=[]
         for el in self.input:
-            dot_val=np.dot(query, el)
-            res.append(dot_val)
+            dot_val=torch.dot(query, el)
+            np_val=dot_val.detach().numpy()
+            np_val=np.float32(np_val)
+            res.append(np_val)
+        res=torch.tensor(res)
         return res
